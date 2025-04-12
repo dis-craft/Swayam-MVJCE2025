7c5ba88bb6ecca9466c7be52f4f73e01f53019a2
Rohan Badmi <rohanbadmi@gmail.com>
2025-04-12 14:46:43 +0530

    Update HeroSection and assets

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,17 +3,60 @@ import React from "react";
 
 const HeroSection = () => {
   return (
-    <div>
-      <div className="relative w-screen h-screen overflow-y-scroll overflow-x-hidden">
+    <div className="relative">
+      <div className="relative w-screen min-h-screen overflow-y-scroll overflow-x-hidden">
+        <Image
+          src="/assets/bgImg.png"
+          alt="Full screen"
+          fill
+          className="object-cover z-10"
+        />
+      </div>
+      <div className="relative w-screen  min-h-screen overflow-y-scroll overflow-x-hidden">
+        <Image
+          src="/assets/bgImg2.png"
+          alt="Full screen"
+          fill
+          className="object-cover z-10"
+        />
+      </div>
+      <div className="relative w-screen  min-h-screen overflow-y-scroll overflow-x-hidden">
+        <Image
+          src="/assets/bgImg3.png"
+          alt="Full screen"
+          fill
+          className="object-cover z-10"
+        />
+      </div>
       <Image
-        src="/assets/bgImg.png"
-        alt="Full screen"
-        fill // shorthand for layout="fill"
-        className="object-cover z-10"
-      />
+        className="absolute top-0 left-82 z-20"
+        src={"/assets/swayamLogo.png"}
+        width={900}
+        height={900}
+        alt="Swayam Logo"
+      ></Image>
+      <Image
+        className="absolute top-[20rem] left-80 z-20"
+        src={"/assets/frontImg.png"}
+        width={900}
+        height={900}
+        alt="Front Image"
+      ></Image>
+      <Image
+        className="absolute top-[62rem] left-100 z-20"
+        src={"/assets/VIRASAT.png"}
+        width={700}
+        height={700}
+        alt="VIRASAT"
+      ></Image>
+      <Image
+        className="absolute top-[70rem] left-100 z-20"
+        src={"/assets/date.png"}
+        width={700}
+        height={700}
+        alt="Date"
+      ></Image>
       <div className="absolute inset-0 bg-gradient-to-b from-[#FCC77B] to-[#FAB34D]/95" />
-    </div>
-      {/* <img src="D:\Swayam-MVJCE2025\public\bgImg.png" className='h-50 w-50' alt="" /> */}
     </div>
   );
 };
